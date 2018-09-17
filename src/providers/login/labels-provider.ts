fbea9e932e2e4bc07fa22551c5b5b6d9ba20403f
isha mehra <isha.mehra@objectconsulting.com.au>
2018-09-17 12:12:14 +1000

    inject ddb token for labels provider

--- src/providers/login/labels-provider.ts
+++ src/providers/login/labels-provider.ts
@@ -1,19 +1,20 @@
 import { Injectable, Inject } from '@angular/core';
 import { AmplifyService } from 'aws-amplify-angular';
 import { AwsProvider } from '../../app/aws-provider';
+import { ddbToken } from '../../shared/shared.module';
 @Injectable()
 export class LabelsProvider{
 
   constructor(public amplifyService: AmplifyService,
-              public awsProvider: AwsProvider
-              ) {
+              public awsProvider: AwsProvider,
+              @Inject(ddbToken) private ddb: any) {
 
   }
 
   getLabelsForUser() {
-    /*const listPromise = this.ddb.listTables({Limit: 10}).promise().then((data) => {
+    const listPromise = this.ddb.listTables({Limit: 10}).promise().then((data) => {
       console.log("got tables from dynamo db " + data);
-    });*/
+    });
   }
 
   getPublicLabels() {
